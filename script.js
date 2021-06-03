2c52324bf1244f595bb37440fc10757e219848e5
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-06-03 15:58:03 -0400

    finished styling , about to start the javaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+//toggle red border and icon for failed input element
+//make sure all inputs are filled out
+// email address must contain .com or .net or .org to be valid
+
+//when the claim your free trial button is clicked - validate the inputs
+
+let alertText = document.querySelectorAll("small");
+let alertIcon = document.querySelectorAll(".alert-img");
